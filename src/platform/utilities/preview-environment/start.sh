a8afef2e552564bb09a8cfc8657b6031884a920c
Peter Hill <pjhill@FlyingDutchman>
2023-05-17 14:13:31 -0400

    copy commands from review entry point script

--- src/platform/utilities/preview-environment/start.sh
+++ src/platform/utilities/preview-environment/start.sh
@@ -25,39 +25,43 @@ else
     git clone -b ${SOURCE_REF} --single-branch https://github.com/department-of-veterans-affairs/vets-website.git
 fi
 
-echo "Download dev content-build to website dir"
+# echo "Download dev content-build to website dir"
 
 # if AWS_URL then use it
 # else use default cache URL
 # To-do -- change the fallback cache to something that is not hardcoded to a file that will eventually be cleaned up
-if [ -z ${AWS_URL} ] ;
-then
-    echo "AWS_URL is NULL; using default" ;
-    curl -LO https://vetsgov-website-builds-s3-upload.s3-us-gov-west-1.amazonaws.com/content-build/0008051ce15e731cc01289933dfb060d6f0d4df6/vagovprod.tar.bz2 ;
-else
-    echo "AWS_URL is not NULL; using workflow env var" ;
-    curl -LO ${AWS_URL} ;
-fi
+# if [ -z ${AWS_URL} ] ;
+# then
+#     echo "AWS_URL is NULL; using default" ;
+#     curl -LO https://vetsgov-website-builds-s3-upload.s3-us-gov-west-1.amazonaws.com/content-build/0008051ce15e731cc01289933dfb060d6f0d4df6/vagovprod.tar.bz2 ;
+# else
+#     echo "AWS_URL is not NULL; using workflow env var" ;
+#     curl -LO ${AWS_URL} ;
+# fi
 
-echo "Setup content-build and extract pre-built content into content-build/build/localhost"
-echo "make the build folder"
-mkdir -p content-build/build/localhost
-echo "untar the build into content-build/build/localhost/"
-tar -xf vagovprod.tar.bz2 -C content-build/build/localhost/
+# echo "Setup content-build and extract pre-built content into content-build/build/localhost"
+# echo "make the build folder"
+# mkdir -p content-build/build/localhost
+# echo "untar the build into content-build/build/localhost/"
+# tar -xf vagovprod.tar.bz2 -C content-build/build/localhost/
 
 echo "set yarn to allow self-signed cert for install"
 yarn config set "strict-ssl" false
 
 # Build and watch vets-website
-echo "Install, build, and watch vets-website"
+# customAPI=http://vets-api-web:3004
+echo "Install and build vets-website with custom api (hardcoded)"
 cd vets-website
-yarn install
-yarn build:webpack:local
-yarn watch --env api="http://vets-api-web:3004" &
+yarn install --production=false
+npm run build -- --buildtype=localhost --api=vets-api-web:3004 --host=localhost --port=3001
 
 # Serve the content-build
-echo "Install and serve content-build"
+echo "Install content-build"
 cd ../content-build
-yarn install
-ln -s /app/website/vets-website/build/localhost/generated /app/website/content-build/build/localhost/generated
-yarn serve
+cp .env.example .env && yarn install --production=false
+echo "Fetch drupal cache"
+npm run fetch-drupal-cache
+echo "Build content-build with custom api (hardcoded)"
+npm run build -- --buildtype=localhost --use-cached-assets --api=vets-api-web:3004 --host=localhost --port=3002
+echo "Serve up content-build"
+npm run heroku-serve -- build/localhost -p 3002
